8d745e2d76de8ebd30ae9af3c669efc07fa6edb4
Vincent Stoessel <xaymaca@gmail.com>
2026-04-01 18:18:31 -0400

    fixing asstes across platforms

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.xaymaca.sit"
         minSdk = 26
         targetSdk = 35
-        versionCode = 7
-        versionName = "1.3.3"
+        versionCode = 8
+        versionName = "1.3.4"
     }
 
     signingConfigs {
