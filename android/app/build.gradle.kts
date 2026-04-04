b27dc1c6b2b5f470d408c96ff22e6ad52e50f3c2
Vincent Stoessel <xaymaca@gmail.com>
2026-04-04 01:12:54 -0400

    build: bump Android version to 1.4.8 (23)

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "com.xaymaca.sit"
         minSdk = 26
         targetSdk = 35
-        versionCode = 21
-        versionName = "1.4.6"
+        versionCode = 23
+        versionName = "1.4.8"
     }
 
     signingConfigs {
